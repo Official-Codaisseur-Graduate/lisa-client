0e4c43e4c1c07e3bb772e164552f9ae80d24ff83
tallybee <talbondareva@gmail.com>
2019-06-26 11:50:36 +0200

    fix type

--- src/Components/DishPicker/DishPicker.js
+++ src/Components/DishPicker/DishPicker.js
@@ -11,7 +11,6 @@ import React from 'react'
 
 function DishPicker(props) {
   const { dishes, types, onChangeType, onChange, onSubmit, dishName } = props
-  console.log("WHAT IS ONCHANGE?!", onChange)
   const typeOptions = types && types.map(type => {
     const { name, id } = type
     return (
