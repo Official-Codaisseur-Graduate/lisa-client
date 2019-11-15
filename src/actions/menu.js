c26a5ed2c916654771531e780ebb303b4cecd46c
dung-phan <madewithcrayons@gmail.com>
2019-11-15 15:40:44 +0100

    fixed deleting items on the menu

--- src/actions/menu.js
+++ src/actions/menu.js
@@ -1,8 +1,8 @@
-import * as request from 'superagent';
-import { baseUrl } from '../constants';
-import { SET_DAY_MENU, SET_WEEK_MENU } from './';
+import * as request from "superagent";
+import { baseUrl } from "../constants";
+import { SET_DAY_MENU, SET_WEEK_MENU, ITEM_MENU_DELETE } from "./";
 
-export const setDayMenu = (menu) => {
+export const setDayMenu = menu => {
   return {
     type: SET_DAY_MENU,
     menu
@@ -10,41 +10,50 @@ export const setDayMenu = (menu) => {
 };
 
 // 5
-export const getDayMenu = (date, locationId) => (dispatch) => {
-  console.log('date here', date)
+export const getDayMenu = (date, locationId) => dispatch => {
+  console.log("date here", date);
   request
-  //add location
+    //add location
     .get(`${baseUrl}/location/${locationId}/menus/?date=${date}`)
     .send(date)
-    .then((res) => {
+    .then(res => {
       dispatch(setDayMenu(res.body));
     });
 };
 
-export const addMenuItem = (dish, locationId) => (dispatch) => {
-  console.log('dish date', dish)
+export const addMenuItem = (dish, locationId) => dispatch => {
+  console.log("dish date", dish);
 
-  request.post(`${baseUrl}/location/${locationId}/menus`).send({ dish}).then((res) => {
-    dispatch(getDayMenu(dish.date, locationId));
-  });
-};
-
-export const deleteMenuItem = (id, date) => (dispatch) => {
-  request.delete(`${baseUrl}/menus/${id}`)
-    .then(() => {
-      return dispatch(getDayMenu(date));
+  request
+    .post(`${baseUrl}/location/${locationId}/menus`)
+    .send({ dish })
+    .then(res => {
+      dispatch(getDayMenu(dish.date, locationId));
     });
 };
+export const deleteMenuItemSuccess = itemId => {
+  return {
+    type: ITEM_MENU_DELETE,
+    itemId
+  };
+};
+export const deleteMenuItem = (id, date) => dispatch => {
+  console.log("check id and date", id, date);
+  request
+    .delete(`${baseUrl}/menus/${id}`)
+    .then(() => dispatch(deleteMenuItemSuccess(id)));
+  // .then(() => dispatch(getDayMenu(date)));
+};
 
-export const setWeekMenu = (menu) => {
+export const setWeekMenu = menu => {
   return {
     type: SET_WEEK_MENU,
     menu
   };
 };
 
-export const getWeekMenu = (date, locationId) => (dispatch) => {
+export const getWeekMenu = (date, locationId) => dispatch => {
   request
     .get(`${baseUrl}/location/${locationId}/menu/week/${date}`)
-    .then((res) => dispatch(setWeekMenu(res.body)));
+    .then(res => dispatch(setWeekMenu(res.body)));
 };
