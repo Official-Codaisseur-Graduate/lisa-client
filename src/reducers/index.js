2c29f332f8c9675731b84b93ef2f2709fbaf00e3
ElizaLU <ELIZA.LUSZCZEWSKA@GMAIL.com>
2019-12-05 17:53:23 +0100

    location actions in one file

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,20 +1,20 @@
-import { combineReducers } from 'redux';
-import type from './type';
-import types from './types';
-import dishes from './dishes';
-import date from './date';
-import menu from './menu';
-import weekMenu from './weekmenu';
-import locationReducer from './locationReducer'
-import currentLocation from './currentLocation'
+import { combineReducers } from "redux";
+import type from "./type";
+import types from "./types";
+import dishes from "./dishes";
+import date from "./date";
+import menu from "./menu";
+import weekMenu from "./weekmenu";
+import locationReducer from "./locationReducer";
+import currentLocation from "./currentLocation";
 
 export default combineReducers({
-	type,
-	types,
-	dishes,
-	date,
-	menu,
-	weekMenu,
-	locationReducer,
-	currentLocation
+  type,
+  types,
+  dishes,
+  date,
+  menu,
+  weekMenu,
+  locationReducer,
+  currentLocation
 });
