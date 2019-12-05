3b38a1f8d3096954f96794dd99d67fcb683f25dc
ElizaLU <ELIZA.LUSZCZEWSKA@GMAIL.com>
2019-12-05 15:57:34 +0100

    location from redux state, get types on mount

--- src/Components/Menu/index.js
+++ src/Components/Menu/index.js
@@ -1,24 +1,19 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import moment from 'moment';
-import Menu from './Menu';
-
 import {setDate} from '../../actions/date';
 import {getDayMenu} from '../../actions/menu';
+import Menu from './Menu';
 
 export class MenuContainer extends Component {
     componentDidMount() {
-        //console.log('loc id params match', this.props.match.params.locationId)
     }
 
     changeDate = (currentDate) => {
-
         const date = moment(currentDate).format('YYYY-MM-DD');
-        // Set date in Redux Store
+        const currentLocation = this.props.currentLocation
         this.props.setDate(date);
-
-        // Get-request to get all dishes for this date
-        this.props.getDayMenu(date, this.props.currentLocationState);
+        this.props.getDayMenu(date, currentLocation );
     };
 
     render() {
@@ -28,7 +23,7 @@ export class MenuContainer extends Component {
 
 const mapStateToProps = (state) => {
     return {
-        currentLocationState : state.currentLocation
+        currentLocation : state.currentLocation
     };
 };
 
