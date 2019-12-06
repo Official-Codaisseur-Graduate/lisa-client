b4047d0e49bad6ef758b71060dbd005654cf4284
sssgordon <gso7@uwo.ca>
2019-12-06 11:56:54 +0100

    Alert and route works

--- src/Components/Location/SelectLocation.js
+++ src/Components/Location/SelectLocation.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { connect } from "react-redux";
 import { getLocations } from "../../actions/locations";
 import { setLocationId } from "../../actions/locations";
 import { getDayMenu } from "../../actions/menu";
+import { Alert, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 class SelectLocation extends React.Component {
+  state = {
+    locationSelected: false
+    // locationUpdated: false
+  };
+
   componentDidMount() {
     this.props.getLocations();
   }
@@ -14,26 +21,53 @@ class SelectLocation extends React.Component {
     let locationId = event.target.options[selectedIndex].getAttribute(
       "data-key"
     );
-    this.props.history.push(`/location/${locationId}`);
+    // this.props.history.push(`/location/${locationId}`);
     this.props.setLocationId(locationId);
     //this.props.getDayMenu(this.props.date.date, locationId);
+    // this.state.locationSelected? this.setState({locationUpdated: true}) : this.setState({locationSelected: true})
+    this.setState({ locationSelected: true });
   };
 
   render() {
-    return (
-      <select onChange={this.updateSelection}>
-        <option value={""}>--Selecteer een locatie--</option>
+    const selectedMessage = this.state.locationSelected && (
+      <Alert
+        variant="success"
+        className="mx-auto"
+        style={{ maxWidth: "500px", minHeight: "150px", margin: "60px auto" }}
+      >
+        <Alert.Heading>Geselecteerde locatie!</Alert.Heading>
+        <hr />
+        <div className="d-flex justify-content-center">
+          <Link to="/createDish">
+            <Button variant="outline-success" style={{ margin: "10px" }}>
+              Gerecht toevoegen
+            </Button>
+          </Link>
+          <Link to="/menu">
+            <Button variant="outline-success" style={{ margin: "10px" }}>
+              Menu maken
+            </Button>
+          </Link>
+        </div>
+      </Alert>
+    );
 
-        {this.props.locations.map(location => (
-          <option
-            key={location.id}
-            data-key={location.id}
-            value={location.name}
-          >
-            {location.name}
-          </option>
-        ))}
-      </select>
+    return (
+      <Fragment>
+        <select onChange={this.updateSelection}>
+          <option value={""}>--Selecteer een locatie--</option>
+          {this.props.locations.map(location => (
+            <option
+              key={location.id}
+              data-key={location.id}
+              value={location.name}
+            >
+              {location.name}
+            </option>
+          ))}
+        </select>
+        {selectedMessage}
+      </Fragment>
     );
   }
 }
@@ -41,7 +75,8 @@ class SelectLocation extends React.Component {
 const mapStateToProps = state => {
   return {
     locations: state.locationReducer,
-    date: state.date
+    date: state.date,
+    locationId: state.currentLocation
   };
 };
 //not sure if need getDayMenu here...
