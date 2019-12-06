bb4b24e4a010bb3fb446640992f7843a27a04e20
ElizaLU <ELIZA.LUSZCZEWSKA@GMAIL.com>
2019-12-06 14:47:48 +0100

    location displayed in navbar

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const allLocations = useSelector(state => state.locationReducer);
+  
+  const currentLocation = useSelector(state => state.currentLocation);
+  console.log("currentLocation", currentLocation);
+  const currLocparsed = parseInt(currentLocation);
+  console.log("currLocparsed", currLocparsed);
+
+  const luck = allLocations.find(location => location.id === currLocparsed);
+  console.log("luck", luck);
+
+  const luckName = luck && true;
+  console.log("luckName", luckName);
+
+  const join = luckName && <p>Your current address is {luck.address}</p>;
+
+  // const addre = luck.name;
+  // console.log("addre", addre);
+
   return (
     <div className="container">
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -20,7 +39,7 @@ const Navbar = () => {
         <Link className="nav-link" to="/weekMenu">
           Week select
         </Link>
-        <p className="navbar-brand">Your location is</p>
+        {join}
       </nav>
     </div>
   );
